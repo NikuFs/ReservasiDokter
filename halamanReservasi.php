9da543a33e3dbe80de4ab13240ae424a656f209d
DESKTOP-75JOBGG\fikri <fikeifikei21@gmail.com>
2023-06-09 21:38:40 +0700

    tambah alert form reservasi

--- halamanReservasi.php
+++ halamanReservasi.php
@@ -1,47 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>Halaman Reservasi</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
+    <title>Halaman Reservasi</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-    <body>
-        <div class="center">
-            <h1>Reservasi</h1>
-            <form action="reservasi.php" method="post">
-                <div class="txt_field">
-                    <input type="text" name="nama" required>
-                    <span></span>
-                    <label>Nama</label>
-                </div>
-                
-                <div class="txt_field">
-                    <input type="date" name="tanggal" required>
-                    <span></span>
-                </div>
-                
-                <div class="txt_field">
-                    <input type="time" name="waktu" required>
-                    <span></span>
-                </div>
-                
-                <div class="txt_field">
-                    <input type="text" name="dokter" required>
-                    <span></span>
-                    <label>Dokter</label>
-                </div>
-                
-                <input type="submit" name="submit" value="Tambah">
-            </form>
+<body>
+    <div class="center">
+        <h1>Reservasi</h1>
+        <form action="reservasi.php" method="post" onsubmit="showAlert()">
+            <div class="txt_field">
+                <input type="text" name="nama" required>
+                <span></span>
+                <label>Nama</label>
+            </div>
 
-            <form action="homePasien.php">
-                <input type="submit" name="submit" value="Back">
+            <div class="txt_field">
+                <input type="date" name="tanggal" required>
+                <span></span>
+            </div>
+
+            <div class="txt_field">
+                <input type="time" name="waktu" required>
+                <span></span>
+            </div>
+
+            <div class="txt_field">
+                <input type="text" name="dokter" required>
+                <span></span>
+                <label>Dokter</label>
+            </div>
+
+            <input type="submit" name="submit" value="Tambah">
         </form>
-        </div>
-    </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+        <form action="homePasien.php">
+            <input type="submit" name="submit" value="Back">
+        </form>
+    </div>
+
+    <script>
+        function showAlert() {
+            alert("Reservasi berhasil dilakukan!");
+        }
+    </script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
 </html>
