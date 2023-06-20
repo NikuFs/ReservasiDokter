0b8044d9bb9ad6b3f40972b2597eeeec2840bc07
Rahmadi Dimas <76185281+Rahmadi16@users.noreply.github.com>
2023-06-20 12:02:59 +0700

    Add files via upload

---
+++ formCheckout.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Halaman Checkout</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+<body>
+  <div class="center">
+    <div class="row justify-content-center">
+      <div class="col-lg-6 px-4 pb-4" id="order">
+        <h1 class="text-center text-info p-2">Checkout Reservasi</h1>
+        <div class="jumbotron p-3 mb-2 text-center">
+        <h5 class="lead"><b>Nama : </b></h5>
+        <h5 class="lead"><b>Usia : </b></h5>
+          <h5 class="lead"><b>Dokter : </b></h5>
+          <h5 class="lead"><b>Tanggal : </b></h5>
+          <h5 class="lead"><b>Jam : </b></h5>
+          <h5><b>Biaya Reservasi : </b>/-</h5>
+        </div>
+        <form action="" method="post" id="placeOrder">
+          <input type="hidden" name="products" value="<?= $allItems; ?>">
+          <input type="hidden" name="grand_total" value="<?= $grand_total; ?>"> 
+          <div class="form-group">
+            <input type="email" name="email" class="form-control" placeholder="Enter E-Mail" required>
+          </div>
+          <div class="form-group">
+            <input type="tel" name="phone" class="form-control" placeholder="Enter Phone" required>
+          </div>
+          <div class="form-group">
+            <textarea name="address" class="form-control" rows="3" cols="50" placeholder="Enter Delivery Address Here..."></textarea>
+          </div>
+          <h6 class="text-center lead">Select Payment Mode</h6>
+          <div class="form-group">
+            <select name="pmode" class="form-control">
+              <option value="" selected disabled>-Select Payment Mode-</option>
+              <option value="cod">Cash On Delivery</option>
+              <option value="netbanking">Net Banking</option>
+              <option value="cards">Debit/Credit Card</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <input type="submit" name="submit" value="Checkout" class="btn btn-danger btn-block">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
