18ab799f1389b89ea0a95de739c31241bd5b747f
OBIT REVIAN OKTIA ZANDRA <ramadhan.r2008@gmail.com>
2023-07-02 20:54:15 +0700

    slebew

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "test";
-
-    //buat koneksi
-    $conn = mysqli_connect($servername, $username, $password, $database);
-?>
